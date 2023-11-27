a924b26abd41bfd24d95888733d7e0fbcc662b0e
MIKE9708 <michelearena46@gmail.com>
2023-11-27 18:07:20 +0100

    Blink only in insert mode

--- init.lua
+++ init.lua
@@ -643,9 +643,9 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.o.guicursor = table.concat({
-  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+  --"n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver30-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  -- "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
 }, ",")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
