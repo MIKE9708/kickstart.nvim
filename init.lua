fa6d89439897b3d17ed7d8ed68a104dfe328f79d
MIKE9708 <michelearena46@gmail.com>
2024-10-06 17:21:13 +0200

    FIXED 4 SPACES AS TAB

--- init.lua
+++ init.lua
@@ -647,10 +647,11 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-local set = vim.opt -- set options
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
+-- local set = vim.opt -- set options
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 vim.o.guicursor = table.concat({
   --"n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
